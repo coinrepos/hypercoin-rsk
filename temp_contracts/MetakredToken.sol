5259008e4cc90bf3857cadca11901b589fdf245d
gamemaster.luckycoin <contact@luckycoin.com>
2025-05-01 20:45:01 +1200

    Add Metakred support files and API bridge

---
+++ temp_contracts/MetakredToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: PROTECTED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MetakredToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Metakred Token", "MTK") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
